0dbaab08c9d91800bc8cc8f71471f318ef0030d1
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2024-01-05 19:13:47 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,6 +1,4 @@
-/**
-* Computes the periodical payment necessary to re-pay a given loan.
-*/
+
 public class LoanCalc {
 	
 	static double epsilon = 0.001;  // The computation tolerance (estimation error)
@@ -40,19 +38,40 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
     	// Replace the following statement with your code
-    	return 0;
+    	double payment = loan/n;
+    	double increment = 0.001;
+    	while (endBalance(loan,rate,n,payment) > epsilon)
+    	{
+    		payment +=increment;
+    		iterationCounter++;
+    	}
+    	return payment;
     }
     
     /**
 	* Uses bisection search to compute an approximation of the periodical payment 
 	* that will bring the ending balance of a loan close to 0.
-	* Given: the sum of theloan, the periodical interest rate (as a percentage),
+	* Given: the sum of the loan, the periodical interest rate (as a percentage),
 	* the number of periods (n), and epsilon, a tolerance level.
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    	double H = loan;
+    	double L = 0;
+        double g = (L+H)/2.0;
+		while ((H - L) > epsilon) {
+            g = (L+H)/2.0;
+			if (endBalance(loan,rate,n,g)> 0)
+			{
+					L = g;
+			}
+		else
+		{
+			H = g;
+		}
+			iterationCounter++;
+		}
+		return g;
     }
 	
 	/**
@@ -60,7 +79,13 @@ public class LoanCalc {
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
+		//function should return thr end balance of the loan 
+		double annual_rate = 1+ (rate/100);
+		for (int i =0; i<n; i++) //enters a for loop for the number of payments
+		{
+			loan = (loan - payment) * annual_rate;
+		}
+		return loan; 
+    	
 	}
 }
